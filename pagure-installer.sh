f6168888e395d096d49f435f82c0548c258a99c3
Luis Guzman <ark@switnet.org>
2019-01-15 21:13:43 -0600

    Changing over to mono user
    Moving to a monolitic user structure

--- pagure-installer.sh
+++ pagure-installer.sh
@@ -28,18 +28,13 @@ apt -yqq install \
 				python3-jinja2 \
 				python3-pip \
 				python3-psycopg2 \
-				redis-server \
-				virtualenv
+				python3-venv \
+				virtualenv \
+				redis-server
 
 echo "gitolite3 gitolite3/adminkey string " | debconf-set-selections
 apt -yqq install gitolite3
-# gitolite > 3.6.4 < 3.7.10 ?
-# https://pagure.io/pagure/issue/3971
-#apt -yqq install \
-#				git \
-#				libjson-perl
-#wget https://ark.switnet.org/tmp/gitolite3/gitolite3_3.6.7-2_all.deb
-#dpkg -i gitolite3_3.6.7-2_all.deb
+
 PGSV=$(apt-cache madison postgresql | head -n1 | awk '{print $3}' | cut -d "+" -f1)
 PYT_V=$(apt-cache madison python3 | head -n1 | awk '{print $3}' | cut -d "." -f1,2)
 install_ifnot() {
@@ -65,34 +60,7 @@ grep -n $1 $2 | cut -d ":" -f1 | head -n1
 #--------------------------------------------------
 install_ifnot language-pack-en-base
 install_ifnot postgresql-$PGSV
-#echo -e "\n---- PostgreSQL Settings  ----"
-#sed -i "s|#listen_addresses = 'localhost'|listen_addresses = '*'|g" /etc/postgresql/$PGSV/main/postgresql.conf
-echo "
-Please select the suffix for this pagure instance.
-"
-#Add what has been used  and such as if empty loop
-echo "Pagure sufix:"
-PAG_USED=$(sudo -u postgres psql -c "SELECT r.rolname as username,r1.rolname as "role" \
- FROM pg_catalog.pg_roles r LEFT JOIN pg_catalog.pg_auth_members m \
- ON (m.member = r.oid) \
- LEFT JOIN pg_roles r1 ON (m.roleid=r1.oid) \
- WHERE r.rolcanlogin \
- ORDER BY 1;" | sed -n '/pag/p' | cut -d " " -f2 | cut -d "_" -f2 | sort -r)
-while [[ -z $sufix ]]
-do
-echo "These have been already taken (avoid them):"
-if [[ -z "$PAG_USED" ]]; then
-	echo " -> Seems there is no other Pagure instance present."
-else
-	echo $PAG_USED
-fi
-read sufix
-if [[ ! -z $sufix ]]; then
-	echo "We'll use sufix \"$sufix\" "
-else
-	echo "Please enter a small sufix for this instance."
-fi
-done
+
 #Enable jenkins?
 while [[ $jenkins != yes && $jenkins != no ]]
 do
@@ -103,18 +71,24 @@ elif [ $jenkins = "yes" ]; then
 	echo "Jenkins will be enabled"
 fi
 done
-PAG_USER="pag_$sufix"
+PAG_ID="pagure"
+PAG_USER="git"
 PAG_PDB="${PAG_USER}_db"
 SHUF=$(shuf -i 15-19 -n 1)
 PDB_PASS=$(tr -dc "a-zA-Z0-9@#*=" < /dev/urandom | fold -w "$SHUF" | head -n 1)
-PAG_HOME="/opt/$PAG_USER"
+PAG_HOME="/home/$PAG_USER"
 PAG_HOME_EXT="$PAG_HOME/$PAG_USER-server"
-PAG_CFG_FILE="$PAG_HOME/pagure.cfg"
 HOOK_RUNR="$PAG_HOME_EXT/pagure/hooks/files/hookrunner"
-PAG_WRK_SRV=/lib/systemd/system/${PAG_USER}-worker.service
-PAG_GIT_WRK=/lib/systemd/system/${PAG_USER}_gitolite_worker.service
-PAG_CI_WRK=/lib/systemd/system/${PAG_USER}_ci.service
-LOG_FILE=$PAG_HOME/log/$PAG_USER-server.log
+PY_VENV_DIR="/home/$PAG_USER/.venv"
+PAG_USR_SHARE="/usr/share/pagure"
+PAG_CFG_DIR="/etc/pagure"
+PAG_CFG_FILE="/etc/pagure/pagure.cfg"
+LOG_DIR="/var/log/pagure"
+LOG_FILE="/var/log/pagure/pagure-server.log"
+PAG_WRK_SRV="/lib/systemd/system/${PAG_ID}_worker.service"
+PAG_GIT_WRK="/lib/systemd/system/${PAG_ID}_gitolite.service"
+PAG_CI_WRK="/lib/systemd/system/${PAG_ID}_ci.service"
+PAG_EV_WRK="/lib/systemd/system/${PAG_ID}_ve.service"
 ADDRESS=$(hostname -I | cut -d ' ' -f 1)
 CERTBOT_REPO=$(apt-cache policy | grep http | grep certbot | head -n 1 | awk '{print $2}' | cut -d "/" -f 5)
 if [ $DIST = xenial ]; then
@@ -175,41 +149,43 @@ echo -e "\n---- Creating $PAG_USER system user ----"
 adduser --system --quiet --shell=/bin/bash --home=$PAG_HOME --gecos 'Pagure' --group $PAG_USER
 usermod -a -G www-data $PAG_USER
 echo -e "\n---- Creating log directory ----"
-mkdir $PAG_HOME/log/
-chown $PAG_USER:$PAG_USER $PAG_HOME/log/
+mkdir $LOG_DIR
+mkdir $PAG_CFG_DIR
+chown $PAG_USER:$PAG_USER $LOG_DIR $PAG_CFG_DIR
 
 #Retrieve the sources
 cd $PAG_HOME
 sudo su $PAG_USER -c "git clone --depth 1 https://pagure.io/pagure.git $PAG_HOME_EXT/"
 sed -i "s|.*sys.path.insert.*|sys.path.insert(0, \'$PAG_HOME_EXT\')|" $HOOK_RUNR
+#Keep for verbose config.
 sed -i "s|/etc/pagure/pagure.cfg|$PAG_CFG_FILE|g" $HOOK_RUNR
-#Apache copy
 echo -e "\n---- Installing python (pip) dependacies for pagure ----"
-cd $PAG_HOME_EXT
-sudo su $PAG_USER -c "virtualenv --system-site-packages -p python3 ./venv"
-sudo su $PAG_USER -c "source ./venv/bin/activate"
-sudo su $PAG_USER -c "$PAG_HOME_EXT/venv/bin/pip3 install --upgrade pip"
-sudo su $PAG_USER -c "$PAG_HOME_EXT/venv/bin/pip3 install pygit2==0.26.4"
-#sudo su $PAG_USER -c "$PAG_HOME_EXT/venv/bin/pip3 install psycopg2 idna==2.7 pygit2==0.26.4"
-sudo su $PAG_USER -c "$PAG_HOME_EXT/venv/bin/pip3 install -r $PAG_HOME_EXT/requirements.txt"
+sudo su $PAG_USER -c "mkdir $PY_VENV_DIR"
+sudo su $PAG_USER -c "virtualenv --system-site-packages -p python3 $PY_VENV_DIR"
+sudo su $PAG_USER -c "source $PY_VENV_DIR/bin/activate"
+sudo su $PAG_USER -c "$PY_VENV_DIR/bin/pip3 install --upgrade pip"
+sudo su $PAG_USER -c "$PY_VENV_DIR/bin/pip3 install pygit2==0.26.4"
+#sudo su $PAG_USER -c "$PY_VENV_DIR/bin/pip3 install psycopg2 idna==2.7 pygit2==0.26.4"
+sudo su $PAG_USER -c "$PY_VENV_DIR/bin/pip3 install -r $PAG_HOME_EXT/requirements.txt"
 #Create the folder that will receive the projects, forks, docs, requests and tickets' git repo
+sudo su $PAG_USER -c "mkdir -p $PAG_HOME/.gitolite/{conf,keydir,logs}"
 sudo su $PAG_USER -c "mkdir -p $PAG_HOME/{repositories,attachments,remotes,releases}"
 sudo su $PAG_USER -c "mkdir -p $PAG_HOME/repositories/{docs,forks,requests,tickets}"
-sudo su $PAG_USER -c "mkdir -p $PAG_HOME/.gitolite/{conf,keydir,logs}"
 #Add empty gitolite
 sudo su $PAG_USER -c "touch $PAG_HOME/.gitolite/conf/gitolite.conf"
 #Setup config files
 sudo su $PAG_USER -c "cp $PAG_HOME_EXT/files/gitolite3.rc $PAG_HOME/.gitolite.rc"
 sudo su $PAG_USER -c "cp $PAG_HOME_EXT/files/pagure.cfg.sample $PAG_CFG_FILE"
-sudo su $PAG_USER -c "cp $PAG_HOME_EXT/files/alembic.ini $PAG_HOME/alembic.ini"
-sudo su $PAG_USER -c "cp $PAG_HOME_EXT/files/pagure.wsgi $PAG_HOME/pagure.wsgi"
-sudo su $PAG_USER -c "cp $PAG_HOME_EXT/files/doc_pagure.wsgi $PAG_HOME/doc_pagure.wsgi"
+sudo su $PAG_USER -c "cp $PAG_HOME_EXT/files/alembic.ini $PAG_CFG_DIR/alembic.ini"
+sudo su $PAG_USER -c "cp $PAG_HOME_EXT/files/pagure.wsgi $PAG_USR_SHARE/pagure.wsgi"
+sudo su $PAG_USER -c "cp $PAG_HOME_EXT/files/doc_pagure.wsgi $PAG_USR_SHARE/doc_pagure.wsgi"
+PAG_USR_SHARE
 #ToDo - Check usage
 sudo su $PAG_USER -c "cp $PAG_HOME_EXT/files/aclchecker.py $PAG_HOME/aclchecker.py"
 sudo su $PAG_USER -c "cp $PAG_HOME_EXT/files/keyhelper.py $PAG_HOME/keyhelper.py"
 sudo su $PAG_USER -c "cp $PAG_HOME_EXT/files/api_key_expire_mail.py $PAG_HOME/api_key_expire_mail.py"
 #Fix shebang for several scripts (pagure.spec)
-sed -e "s|#\!/usr/bin/env python|#\!${PAG_HOME_EXT}/venv/bin/python3|" -i \
+sed -e "s|#\!/usr/bin/env python|#\!${PY_VENV_DIR}/bin/python3|" -i \
     $PAG_HOME/aclchecker.py \
     $PAG_HOME/keyhelper.py \
     $PAG_HOME/api_key_expire_mail.py \
@@ -335,7 +311,9 @@ done
 
 #REDIS
 #Using mured.sh - https://github.com/switnet-ltd/mured
-export RED_SUFIX=$sufix
+echo "Redis sufix:"
+read RED_SUFIX
+export RED_SUFIX
 export RED_CON=1
 bash <(curl -s https://raw.githubusercontent.com/switnet-ltd/mured/master/mured.sh)
 #Find port from sufix
@@ -378,18 +356,18 @@ sudo su $PAG_USER -c "ssh-keygen -t rsa -b 4096"
 sudo su $PAG_USER -c "touch $PAG_HOME/.ssh/authorized_keys"
 
 #Create the inital database scheme
-sed -i "s|.*script_location.*|script_location = $PAG_HOME_EXT/alembic|" $PAG_HOME/alembic.ini
-sudo su $PAG_USER -c "$PAG_HOME_EXT/venv/bin/python3 $PAG_HOME_EXT/createdb.py -i $PAG_HOME/alembic.ini -c $PAG_CFG_FILE"
+sed -i "s|.*script_location.*|script_location = $PAG_HOME_EXT/alembic|" $PAG_CFG_DIR/alembic.ini
+sudo su $PAG_USER -c "$PY_VENV_DIR/bin/python3 $PAG_HOME_EXT/createdb.py -i $PAG_CFG_DIR/alembic.ini -c $PAG_CFG_FILE"
 
 ## Setup WSGI
-sed -i "s|/etc/pagure/pagure.cfg|$PAG_CFG_FILE|" $PAG_HOME/pagure.wsgi
-sed -i "s|/path/to/pagure/|$PAG_HOME_EXT/|" $PAG_HOME/pagure.wsgi
-sed -i "s|#import|import|" $PAG_HOME/pagure.wsgi
-sed -i "s|#sys.|sys.|" $PAG_HOME/pagure.wsgi
-sed -i "s|/etc/pagure/pagure.cfg|$PAG_CFG_FILE|" $PAG_HOME/doc_pagure.wsgi
-sed -i "s|/path/to/pagure/|$PAG_HOME_EXT/|" $PAG_HOME/doc_pagure.wsgi
-sed -i "s|#import|import|" $PAG_HOME/doc_pagure.wsgi
-sed -i "s|#sys.|sys.|" $PAG_HOME/doc_pagure.wsgi
+sed -i "s|/etc/pagure/pagure.cfg|$PAG_CFG_FILE|" $PAG_USR_SHARE/pagure.wsgi
+sed -i "s|/path/to/pagure/|$PAG_HOME_EXT/|" $PAG_USR_SHARE/pagure.wsgi
+sed -i "s|#import|import|" $PAG_USR_SHARE/pagure.wsgi
+sed -i "s|#sys.|sys.|" $PAG_USR_SHARE/pagure.wsgi
+sed -i "s|/etc/pagure/pagure.cfg|$PAG_CFG_FILE|" $PAG_USR_SHARE/doc_pagure.wsgi
+sed -i "s|/path/to/pagure/|$PAG_HOME_EXT/|" $PAG_USR_SHARE/doc_pagure.wsgi
+sed -i "s|#import|import|" $PAG_USR_SHARE/doc_pagure.wsgi
+sed -i "s|#sys.|sys.|" $PAG_USR_SHARE/doc_pagure.wsgi
 ##Setup Apache & SSL (if requierements on place)
 if [ ! -z "$APP_URL" ] && [ ! -z "$DOC_APP_URL" ]; then
 echo -e "\nDo you want to setup apache config? (yes|no)"
@@ -408,12 +386,12 @@ echo -e "\nDo you want to setup apache config? (yes|no)"
 			AP2CONF="/etc/apache2/sites-available/$APP_URL.conf"
 			sed -i "s|run/wsgi|/var/run/wsgi|" $AP2CONF
 			sed -i "s|=git|=$PAG_USER|g" $AP2CONF
-			sed -i "/WSGIDaemonProcess/ s|display-name=pagure|display-name=$PAG_USER|" $AP2CONF
-			sed -i "/WSGIDaemonProcess/ s|$| python-home=$PAG_HOME_EXT/venv|" $AP2CONF
+			sed -i "/WSGIDaemonProcess/ s|display-name=pagure|display-name=$PAG_ID|" $AP2CONF
+			sed -i "/WSGIDaemonProcess/ s|$| python-home=$PY_VENV_DIR|" $AP2CONF
 			sed -i "s|localhost.localdomain|$APP_URL|" $AP2CONF
 			sed -i "s|docs.localhost.localdomain|$DOC_APP_URL|" $AP2CONF
-			sed -i "s|/usr/share/pagure/pagure.wsgi|$PAG_HOME/pagure.wsgi|" $AP2CONF
-			sed -i "s|/usr/share/pagure/doc_pagure.wsgi|$PAG_HOME/doc_pagure.wsgi|" $AP2CONF
+			sed -i "s|/usr/share/pagure/pagure.wsgi|$PAG_USR_SHARE/pagure.wsgi|" $AP2CONF
+			sed -i "s|/usr/share/pagure/doc_pagure.wsgi|$PAG_USR_SHARE/doc_pagure.wsgi|" $AP2CONF
 			sed -i "s|/usr/lib/pythonX.Y/site-packages/pagure/static/|$PAG_HOME_EXT/pagure/static/|" $AP2CONF
 			sed -i "s|/var/www/releases|$PAG_HOME/releases|" $AP2CONF
 			sed -i "s|/path/to/git/repositories|$PAG_HOME/repositories|" $AP2CONF
@@ -423,8 +401,8 @@ echo -e "\nDo you want to setup apache config? (yes|no)"
 			service apache2 stop
 			letsencrypt certonly --standalone --renew-by-default --agree-tos --email $ADMIN_MAIL -d $APP_URL
 			letsencrypt certonly --standalone --renew-by-default --agree-tos --email $ADMIN_MAIL -d $DOC_APP_URL
-			set_ssl_apache "$PAG_HOME/pagure.wsgi" $AP2CONF $APP_URL
-			set_ssl_apache "$PAG_HOME/doc_pagure.wsgi" $AP2CONF $DOC_APP_URL
+			set_ssl_apache "$PAG_USR_SHARE/pagure.wsgi" $AP2CONF $APP_URL
+			set_ssl_apache "$PAG_USR_SHARE/doc_pagure.wsgi" $AP2CONF $DOC_APP_URL
 			a2ensite $APP_URL.conf
 			#TuneUp site performance
 			sed -i "s|.*SESSION_COOKIE_SECURE.*|SESSION_COOKIE_SECURE = True|" $PAG_CFG_FILE
@@ -451,22 +429,22 @@ echo -e "\nDo you want to setup apache config? (yes|no)"
 			AP2CONF="/etc/apache2/sites-available/$APP_URL.conf"
 			sed -i "s|run/wsgi|/var/run/wsgi|" $AP2CONF
 			sed -i "s|=git|=$PAG_USER|g" $AP2CONF
-			sed -i "/WSGIDaemonProcess/ s|display-name=pagure|display-name=$PAG_USER|" $AP2CONF
-			sed -i "/WSGIDaemonProcess/ s|$| python-home=$PAG_HOME_EXT/venv|" $AP2CONF
+			sed -i "/WSGIDaemonProcess/ s|display-name=pagure|display-name=$PAG_ID|" $AP2CONF
+			sed -i "/WSGIDaemonProcess/ s|$| python-home=$PY_VENV_DIR|" $AP2CONF
 			sed -i "s|localhost.localdomain|$APP_URL|" $AP2CONF
-			sed -i "s|/usr/share/pagure/pagure.wsgi|$PAG_HOME/pagure.wsgi|" $AP2CONF
+			sed -i "s|/usr/share/pagure/pagure.wsgi|$PAG_USR_SHARE/pagure.wsgi|" $AP2CONF
 			sed -i "s|/usr/lib/pythonX.Y/site-packages/pagure/static/|$PAG_HOME_EXT/pagure/static/|" $AP2CONF
 			sed -i "s|/var/www/releases|$PAG_HOME/releases|" $AP2CONF
 			sed -i "s|/path/to/git/repositories|$PAG_HOME/repositories|" $AP2CONF
 			sed -i "s|#||" $AP2CONF
 			sed -i "s|SSLCertificate|#SSLCertificate|" /etc/apache2/sites-available/$APP_URL.conf
 			sed -i '/<VirtualHost/{:a;N;/\/VirtualHost>/!ba};'"/docs.${APP_URL}/"'d' $AP2CONF
-			sed -i "s|.*WSGIDaemonProcess paguredocs|#WSGIDaemonProcess paguredocs|" $AP2CONF
-			#sed -i "s|.*WSGIDaemonProcess paguredocs:*||" $AP2CONF #comment or delete?
+			  sed -i "s|.*WSGIDaemonProcess paguredocs|#WSGIDaemonProcess paguredocs|" $AP2CONF
+			  #sed -i "s|.*WSGIDaemonProcess paguredocs:*||" $AP2CONF #comment or delete?
 			# Get ssl cert for domain using letsencrypt
 			service apache2 stop
 			letsencrypt certonly --standalone --renew-by-default --agree-tos --email $ADMIN_MAIL -d $APP_URL
-			set_ssl_apache "$PAG_HOME/pagure.wsgi" $AP2CONF $APP_URL
+			set_ssl_apache "$PAG_USR_SHARE/pagure.wsgi" $AP2CONF $APP_URL
 			a2ensite $APP_URL.conf
 			#TuneUp site performance
 			sed -i "s|.*SESSION_COOKIE_SECURE.*|SESSION_COOKIE_SECURE = True|" $PAG_CFG_FILE
@@ -493,9 +471,9 @@ WorkingDirectory=$PAG_HOME_EXT
 SyslogIdentifier=$PAG_USER
 PIDFile=/run/$PAG_USER/$PAG_USER.pid
 ExecStartPre=/usr/bin/install -d -m755 -o $PAG_USER -g $PAG_USER /run/$PAG_USER
-ExecStart=$PAG_HOME_EXT/venv/bin/celery worker -A pagure.lib.tasks_services --loglevel=info
-Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PAG_HOME_EXT/venv/bin"
-Environment="PYTHONPATH=$PAG_HOME_EXT/venv/lib/python${PYT_V}/site-packages"
+ExecStart=$PY_VENV_DIR/bin/celery worker -A pagure.lib.tasks_services --loglevel=info
+Environment="PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PY_VENV_DIR/bin"
+Environment="PYTHONPATH=$PY_VENV_DIR/lib/python${PYT_V}/site-packages"
 Environment="PAGURE_CONFIG=$PAG_CFG_FILE"
 ExecStop=/bin/kill -s TERM \$MAINPID
 
@@ -504,44 +482,60 @@ WantedBy=multi-user.target
 Alias=${PAG_USER}_worker.service
 CELERY
 systemctl enable $PAG_WRK_SRV
-systemctl start ${PAG_USER}_worker.service
+systemctl start ${PAG_ID}_worker.service
 
 #Set gitolite worker
 cp $PAG_HOME_EXT/files/pagure_gitolite_worker.service $PAG_GIT_WRK
 SRV_GW=$(grep -n "\[Service\]" $PAG_GIT_WRK  | cut -d ":" -f1)
 WKD_LIN=$((SRV_GW + 1))
 sed -i "${WKD_LIN}i WorkingDirectory=$PAG_HOME_EXT" $PAG_GIT_WRK
-sed -i "s|/usr/bin/celery|$PAG_HOME_EXT/venv/bin/celery|" $PAG_GIT_WRK
+sed -i "s|/usr/bin/celery|$PY_VENV_DIR/bin/celery|" $PAG_GIT_WRK
 sed -i "s|=git|=$PAG_USER|" $PAG_GIT_WRK
 sed -i "s|=/etc/pagure/pagure.cfg|=$PAG_CFG_FILE|" $PAG_GIT_WRK
 sed -i "/Environment/i \
-Environment=\"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PAG_HOME_EXT/venv/bin\" \\
+Environment=\"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PY_VENV_DIR/bin\" \\
 \
-Environment=\"PYTHONPATH=$PAG_HOME_EXT/venv/lib/python${PYT_V}/site-packages\"" $PAG_GIT_WRK
+Environment=\"PYTHONPATH=$PY_VENV_DIR/lib/python${PYT_V}/site-packages\"" $PAG_GIT_WRK
 systemctl enable $PAG_GIT_WRK
-systemctl start ${PAG_USER}_gitolite_worker.service
+systemctl start ${PAG_ID}_gitolite.service
 
 if [ $jenkins = 'yes' ];then
 echo "Setting up Jenkins"
-sudo su $PAG_USER -c "$PAG_HOME_EXT/venv/bin/pip3 install -r $PAG_HOME_EXT/requirements-ci.txt"
+sudo su $PAG_USER -c "$PY_VENV_DIR/bin/pip3 install -r $PAG_HOME_EXT/requirements-ci.txt"
+sudo su $PAG_USER -c "$PY_VENV_DIR/bin/pip3 install -r $PAG_HOME_EXT/requirements-ev.txt"
+#PAGURE-CI
 cp $PAG_HOME_EXT/files/pagure_ci.service $PAG_CI_WRK
 SRV_GW=$(grep -n "\[Service\]" $PAG_CI_WRK  | cut -d ":" -f1)
 WKD_LIN=$((SRV_GW + 1))
 sed -i "${WKD_LIN}i WorkingDirectory=$PAG_HOME_EXT" $PAG_CI_WRK
-sed -i "s|/usr/bin/celery|$PAG_HOME_EXT/venv/bin/celery|" $PAG_CI_WRK
+sed -i "s|/usr/bin/celery|$PY_VENV_DIR/bin/celery|" $PAG_CI_WRK
 sed -i "s|=git|=$PAG_USER|" $PAG_CI_WRK
 sed -i "s|=/etc/pagure/pagure.cfg|=$PAG_CFG_FILE|" $PAG_CI_WRK
 sed -i "/Environment/i \
-Environment=\"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PAG_HOME_EXT/venv/bin\" \\
+Environment=\"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PY_VENV_DIR/bin\" \\
 \
-Environment=\"PYTHONPATH=$PAG_HOME_EXT/venv/lib/python${PYT_V}/site-packages\"" $PAG_CI_WRK
+Environment=\"PYTHONPATH=$PY_VENV_DIR/lib/python${PYT_V}/site-packages\"" $PAG_CI_WRK
+systemctl enable $PAG_CI_WRK
+systemctl start ${PAG_ID}_ci.service
+#PAGURE-EV
+cp $PAG_HOME_EXT/pagure-ev/pagure_ev.service. $PAG_EV_WRK
+SRV_GW=$(grep -n "\[Service\]" $PAG_EV_WRK  | cut -d ":" -f1)
+WKD_LIN=$((SRV_GW + 1))
+sed -i "${WKD_LIN}i WorkingDirectory=$PAG_HOME_EXT" $PAG_EV_WRK
+sed -i "s|ExecStart=.*|ExecStart=$PAG_HOME_EXT/pagure-ev/pagure_stream_server.py|" $PAG_EV_WRK
+sed -i "s|=git|=$PAG_USER|" $PAG_EV_WRK
+sed -i "s|=/etc/pagure/pagure.cfg|=$PAG_CFG_FILE|" $PAG_EV_WRK
+sed -i "/Environment/i \
+Environment=\"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PY_VENV_DIR/bin\" \\
+\
+Environment=\"PYTHONPATH=$PY_VENV_DIR/lib/python${PYT_V}/site-packages\"" $PAG_EV_WRK
+systemctl enable $PAG_EV_WRK
+systemctl start ${PAG_ID}_ev.service
 cat  << PAG_CI >> $PAG_CFG_FILE
 
 PAGURE_CI_SERVICES = ['jenkins']
 PAG_CI
 
-systemctl enable $PAG_CI_WRK
-systemctl start ${PAG_USER}_ci.service
 fi
 #Clean unused packages
 apt -y autoremove
